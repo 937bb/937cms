098df13784c2f14157f75c0c913307b6df092051
937bb <937bb@example.com>
2025-12-25 23:16:07 +0800

    Remove duplicate migration - cron field already in schema.sql

--- apps/api/sql/migrations/001_add_cron_field.sql
+++
@@ -1,2 +0,0 @@
--- Add cron field to bb_collect_job table if it doesn't exist
-ALTER TABLE `bb_collect_job` ADD COLUMN `cron` varchar(64) NOT NULL DEFAULT '' COMMENT 'cron expression for scheduling' AFTER `filter_keywords`;
