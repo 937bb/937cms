95c13d64d95b27064043f1da65a8c362efe14d76
937bb <937bb@example.com>
2025-12-26 11:13:18 +0800

    使用条件语句删除 vod_play_url 字段
    - 使用 INFORMATION_SCHEMA 检查列是否存在
    - 只在列存在时执行删除操作
    - 兼容所有 MySQL 版本

--- apps/api/sql/migrations/005_remove_vod_play_url.sql
+++ apps/api/sql/migrations/005_remove_vod_play_url.sql
@@ -1,1 +1,5 @@
-ALTER TABLE `bb_vod` DROP COLUMN IF EXISTS `vod_play_url`;
+SET @col_exists = (SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'bb_vod' AND COLUMN_NAME = 'vod_play_url' AND TABLE_SCHEMA = DATABASE());
+SET @sql = IF(@col_exists > 0, 'ALTER TABLE `bb_vod` DROP COLUMN `vod_play_url`', 'SELECT 1');
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
